0ce8b3f70ad955be57031db24bb0f8aecd02acbe
Ulises Desentis <ulifly@hotmail.com>
2021-10-04 15:25:23 -0500

    condiciones 2

--- condiciones2.rb
+++ condiciones2.rb
@@ -1,11 +1,21 @@
 
-condicion = 25
+condicion = 42
+
 
 if condicion < 20
  puts "menor a 20"
 
 elsif condicion < 30
   puts "mayor a 20 menor a 30"
+
+elsif condicion < 40
+  puts "mayor a 30 menor a 40"
+
+elsif condicion < 50
+    puts "mayor a 40 menor a 50"
+
+elsif condicion < 60
+    puts "mayor a 50 menor a 60"
 
 else
  puts "no se cumnple con la con la condicion"
