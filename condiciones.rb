122118ba0c03fb7e278ff91498bd1c586862d575
Ulises Desentis <ulifly@hotmail.com>
2021-10-07 11:30:45 -0500

    actualizacion

--- condiciones.rb
+++ condiciones.rb
@@ -3,7 +3,7 @@ condicion = 890
 
 if condicion >= 20
   puts "se cumple la condicion"
-#else
-#  puts "no se cumnple con la con la condicion"
+else
+  puts "no se cumnple con la con la condicion"
 
 end
